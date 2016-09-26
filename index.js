03131bd1c77df7ded23edfda4a2c6d8a4d2475af
Herock Xia <herock@gmail.com>
2016-09-26 20:49:53 +0800

    update index

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var app = require('express')(),
+    conf = require('.lib/config'),
+    wechat = require('.lib/wechat'),
     bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({
   extended: false
-}))
+}));
+
+wechat(conf.wechat);
+
+wechat.createMenu(require('./lib/menu.json'));
 
 app.use('/wxapi', require('./lib/routers/wxapi.js'));
 
